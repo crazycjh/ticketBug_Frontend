8490433edcf978b23bd97f132824ffcbdddb3115
ChenHao Chen <crazycjh@gmail.com>
2023-12-26 23:45:46 +0800

    await時的等待畫面

---
+++ src/utility/skeleton.vue
@@ -0,0 +1,10 @@
+<template>
+    <SkeletonLoading class="mb-2"></SkeletonLoading>
+    <SkeletonLoading width="10rem" class="mb-2"></SkeletonLoading>
+    <SkeletonLoading width="5rem" class="mb-2"></SkeletonLoading>
+    <SkeletonLoading height="2rem" class="mb-2"></SkeletonLoading>
+    <SkeletonLoading width="10rem" height="4rem"></SkeletonLoading>
+</template>
+<script setup>
+
+</script>
